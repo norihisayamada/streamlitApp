a745193d6d43cd54581a2ce99682ae61ce2bdeb5
norihisayamada <sus444norihisa@gmail.com>
2025-08-30 14:49:41 +0900

    Update2 main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import time
 import pandas as pd
 import streamlit as st
 from numpy.random import default_rng as rng
 
-df1 = pd.DataFrame(
-    rng(0).standard_normal(size=(50, 20)), columns=("col %d" % i for i in range(20))
+df = pd.DataFrame(
+    {
+        "col1": rng(0).standard_normal(1000) / 50 + 37.76,
+        "col2": rng(1).standard_normal(1000) / 50 + -122.4,
+        "col3": rng(2).standard_normal(1000) * 100,
+        "col4": rng(3).standard_normal((1000, 4)).tolist(),
+    }
 )
 
-df2 = pd.DataFrame(
-    rng(1).standard_normal(size=(50, 20)), columns=("col %d" % i for i in range(20))
-)
-
-my_table = st.table(df1)
-time.sleep(1)
-my_table.add_rows(df2)
+st.map(df, latitude="col1", longitude="col2", size="col3", color="col4")
